d67af085827a13783301bf2a0a6d64a495a5bccb
Tavo Cruz <madecruz2@gmail.com>
2023-10-04 14:18:19 -0700

    complex State way 2

--- src/Components/EvenHandling.js
+++ src/Components/EvenHandling.js
@@ -9,21 +9,19 @@ const EvenHandling = () => {
   });
   
   function handleChange(event) {
-    const newValue = event.target.value;
-    const inputName = event.target.name;
 
-    
+    const { value, name } = event.target;
 
     setFullName(prevValue => {
-      if (inputName === "fName") {
+      if (name === "fName") {
         return {
-          fName: newValue,
+          fName: value,
           lName: prevValue.lName
         };
-      } else if (inputName === "lName") {
+      } else if (name === "lName") {
         return {
           fName: prevValue.fName,
-          lName: newValue
+          lName: value
         }
       }
     })
