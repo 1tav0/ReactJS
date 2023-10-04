38173ca0795ba72f0bffa880f5293026491b2df9
Tavo Cruz <madecruz2@gmail.com>
2023-10-03 17:35:19 -0700

    declaritive vs imperative programming

--- src/Components/State.js
+++ src/Components/State.js
@@ -1,11 +1,32 @@
 import React from 'react'
-
+/***DECLARITIVE***/
+/*
 const State = () => {
-  var isDone = true;
+  var isDone = false;
 
   const strikeThrough = { textDecoration: "line-through"}
   return (
     <p style={isDone ? strikeThrough : null}>Buy Milk</p>
+  )
+}
+*/
+/***IMPERATIVE***/
+const State = () => {
+  function strike() {
+    document.getElementById("root").style.textDecoration = 'line-through';
+  }
+
+  function unStrike() {
+    document.getElementById("root").style.textDecoration = null;
+  }
+
+  const strikeThrough = { textDecoration: "line-through"}
+  return (
+    <div>
+      <p>Buy milk</p>
+      <button onClick={strike}>Change to strike through</button>
+      <button onClick={unStrike}>Change back</button>
+    </div>
   )
 }
 
