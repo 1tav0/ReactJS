fa9eef1055a47115ddfa22231aa7f85e2dfdcae6
Tavo Cruz <madecruz2@gmail.com>
2023-10-04 15:36:35 -0700

    todo list app with useState and spread operator along with map

--- src/Components/Spread.js
+++ src/Components/Spread.js
@@ -1,29 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Spread = () => {
-  const citrus = ["lime", "lemon", "orange"];
-  //const fruits = ["apple", "Banana", "coconut", ...citrus];
-  //['apple', 'Banana', 'coconut', 'lime', 'lemon', 'orange']
-  const fruits = ["apple", ...citrus, "Banana", "coconut"];
-  //['apple', 'lime', 'lemon', 'orange', 'Banana', 'coconut']
-  
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
 
-  const fullName = {
-    fName: "James",
-    lName: "Bond"
-  };
-
-  const user = {
-    ...fullName,
-    id: 1,
-    userName: "007"
+  function handleChange(event){
+    const newValue = event.target.value;
+    setInputText(newValue);
   }
-  //{fName: 'James', lName: 'Bond', id: 1, userName: '007'}
 
-  console.log(user)
+  function addItem() {
+    setItems((prevItems) => { 
+      //we return a new array with the previous items and what we have in the input from the user
+      return [...prevItems, inputText]
+    })
+
+    //to clear what we type
+    setInputText("");
+  }
 
   return (
-    <div>Spread</div>
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input
+          type="text"
+          onChange={handleChange}
+          value={inputText}
+        />
+        <button
+          onClick={addItem}
+        >
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((newItem) => (<li>{newItem}</li>))}
+        </ul>
+      </div>
+    </div>
   )
 }
 
