c943598214382ae32cd607fb92c4a4c949c1e9f6
Tavo Cruz <madecruz2@gmail.com>
2023-10-04 13:41:32 -0700

    More complex useState

--- src/Components/EvenHandling.js
+++ src/Components/EvenHandling.js
@@ -3,32 +3,37 @@ import './EvenHandling.css'
 
 const EvenHandling = () => {
 
-  const [name, setName] = useState("");
+  const [fName, setfName] = useState("");
+  const [lName, setlName] = useState("");
 
-  function handleChange(event) {
-    console.log(event.target.value);
-    // console.log(event.target.placeholder);
-    // console.log(event.target.type);
-    setName(event.target.value);
+  function updateFName(event) {
+    const firstName = event.target.value;
+    setfName(firstName);
   }
 
-  const [heading, setHeading] = useState("");
-  function handleClick(event) {
-    setHeading(name);
-
-    event.preventDefault();
+  function updateLName(event) {
+    const lastName = event.target.value;
+    setlName(lastName);
   }
+
   
   return (
     <div className='container'>
-      <h1>Hello {heading}</h1>
-      <form onSubmit={handleClick}>
+      <h1>Hello {fName} {lName}</h1>
+      <form className='form'>
         <input
-          onChange={handleChange}
-          value={name}
-          type="text" placeholder="What's your name?" />
+          name="fName"
+          type="text" placeholder="First name?"
+          value={fName}
+          onChange={updateFName}
+        />
+        <input
+          name="fName"
+          type="text" placeholder="First name?"
+          value={lName}
+          onChange={updateLName}
+        />
         <button
-          type="submit"
         >
           Submit
         </button>
