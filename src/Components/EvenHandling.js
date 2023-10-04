dbc152fdf60b0ebdc4839cac6c2e12d7cf24863d
Tavo Cruz <madecruz2@gmail.com>
2023-10-04 14:32:00 -0700

    changin complex state practice

--- src/Components/EvenHandling.js
+++ src/Components/EvenHandling.js
@@ -2,49 +2,60 @@ import React, { useState } from 'react'
 import './EvenHandling.css'
 
 const EvenHandling = () => {
-
-  const [fullName, setFullName] = useState({
+  const [contact, setContact] = useState({
     fName: "",
-    lName: ""
-  });
-  
-  function handleChange(event) {
+    lName: "",
+    email: ""
+  })
 
+  function handleChange(event) {
     const { value, name } = event.target;
 
-    setFullName(prevValue => {
+    setContact(prevValue => {
       if (name === "fName") {
         return {
           fName: value,
-          lName: prevValue.lName
-        };
+          lName: prevValue.lName,
+          email: prevValue.email
+        }
       } else if (name === "lName") {
         return {
           fName: prevValue.fName,
-          lName: value
+          lName: value,
+          email: prevValue.email
+        }
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value
         }
       }
     })
-
   }
-
-
   
   return (
     <div className='container'>
-      <h1>Hello {fullName.fName} {fullName.lName}</h1>
+      <h1>Hello {contact.fName} {contact.lName}</h1>
+      <p>{contact.email}</p>
       <form className='form'>
         <input
           name="fName"
-          type="text" placeholder="First name?"
-          value={fullName.fName}
+          type="text" placeholder="First Name?"
           onChange={handleChange}
+          value={contact.fName}
         />
         <input
-          name="lName"
-          type="text" placeholder="First name?"
-          value={fullName.lName}
+          name = "lName"
+          type="text" placeholder="Last Name?"
           onChange={handleChange}
+          value={contact.lName}
+        />
+        <input
+          name = "email"
+          type="text" placeholder="Email"
+          onChange={handleChange}
+          value={contact.email}
         />
         <button
         >
